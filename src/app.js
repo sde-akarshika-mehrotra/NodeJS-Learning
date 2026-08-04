6c9cba6589a044580930de2c9d7cd7c22956c580
Akarshika Mehrotra <akarshikamehrotra44@gmail.com>
2026-08-04 23:47:11 +0530

    New updates

--- src/app.js
+++ src/app.js
@@ -16,6 +16,10 @@ app.use(
   }),
 );
 
+import healthCheckRouter from "./routes/healthcheck.routes.js";
+
+app.use("/api/v1/healthcheck", healthCheckRouter);
+
 app.get("/", (req, res) => {
   res.send("Welcome to basecampy");
 });
--- src/index.js
+++ src/index.js
@@ -8,9 +8,6 @@ dotenv.config({
 
 const port = process.env.PORT || 3000;
 
-// app.listen(port, () => {
-//   console.log(`Example app listening on port http://localhost:${port}`);
-// });
 
 connectDB()
   .then(() => {
